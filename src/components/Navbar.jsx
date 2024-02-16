c6fa31009dfee938d65715d5d9718608ae4ee4e6
omarzdat <omarz3idat@gmail.com>
2024-02-16 23:55:53 +1100

    added logo to navbar, facing issues

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,13 @@
 import { NavLink } from "react-router-dom";
 
+import { logo } from "src/assets/images";
+
 const Navbar = () => {
   return (
     <header className='header'>
+      <NavLink to='/'>
+        <img src={logo} alt='logo' className='w-18 h-18 object-contain' />
+      </NavLink>
       <nav className='flex text-lg gap-7 font-medium'>
         <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-600" : "text-black" }>
           About
