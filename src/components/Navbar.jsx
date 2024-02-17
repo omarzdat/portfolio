7e28e421d384c7e9c92f693dab1195d46d2017e5
omarzdat <omarz3idat@gmail.com>
2024-02-17 23:51:17 +1100

    experiment with colors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,10 +9,10 @@ const Navbar = () => {
         <img src={logo} alt='logo' className='w-18 h-18 object-contain' />
       </NavLink>
       <nav className='flex text-lg gap-7 font-medium'>
-        <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-600" : "text-black" }>
+        <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-400" : "text-black" }>
           About
         </NavLink>
-        <NavLink to='/projects' className={({ isActive }) => isActive ? "text-blue-600" : "text-black"}>
+        <NavLink to='/projects' className={({ isActive }) => isActive ? "text-blue-400" : "text-black"}>
           Projects
         </NavLink>
       </nav>
